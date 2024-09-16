47a49dee878dd2d37d7efa3baa46849ebeed7265
Agustin Rojas <agus.rojasmolina@gmail.com>
2024-09-15 21:27:07 -0300

    updates

--- src/components/atoms/CategoryTitle.tsx
+++ src/components/atoms/CategoryTitle.tsx
@@ -1,13 +1,15 @@
 
 interface childrenProps {
     children: JSX.Element | String;
+    text?: string;
+    
 }
 
-export default function CategoryTitle({ children }: childrenProps) {
+export default function CategoryTitle({ children, text = 'text-dark' }: childrenProps) {
     return (
         <div className="my-1">
             <div className="">
-                <h2 className="font-chrono text-[1.9rem] antialiased leading-4 tracking-wider text-dark">{children}</h2>
+                <h2 className={`font-chrono text-[1.9rem] antialiased leading-4 tracking-wider ${text}`}>{children}</h2>
             </div>
         </div>
     )
--- src/components/organisms/SideMenu.tsx
+++ src/components/organisms/SideMenu.tsx
@@ -49,30 +49,42 @@ function SideMenu({ blogEntries }: { blogEntries: any }) {
         <>
             <aside
                 ref={wrapperRef}
-                className={`${showSideMenu ? "!block showSideMenu" : "w-0.5"} ${hideSideMenuAnimation ? "hideSideMenu" : ""}  z-30 h-full max-h-[100vh] laptop:w-[26vw] desktop:w-[21rem] fixed top-0 left-0 bg-cerise  background_noise_overlay`}> {/*  overflow-y-scroll  */}
+                className={`${showSideMenu ? "!block showSideMenu" : "w-0.5"} ${hideSideMenuAnimation ? "hideSideMenu" : ""} overflow-scroll  z-30 h-full max-h-[100vh] laptop:w-[26vw] desktop:w-[21rem] fixed top-0 left-0 bg-cerise  background_noise_overlay`}> {/*  overflow-y-scroll  */}
                 <div className='mx-3 border-b-[1px] h-[54px]  border-timberwolf dark:border-timberwolf border-opacity-50 dark:border-opacity-70 flex'>
                     {/* Empty Space for the logo */}
                 </div>
-
-                <ul className={`${showSideMenu ? 'relative' : 'hidden tablet:block'} p-1 ml-3 mt-4 h-full w-full ${showProjectList ? 'opacity-100' : 'opacity-0'} laptop:!opacity-100 duration-300 transition-all`}>
-                    {POST_CATEGORIES.map(cat => (
-                        <div key={cat.title} className="my-6">
-                            <CategoryTitle>{cat.title}</CategoryTitle>
-                            {POSTS[cat.title].map(POST => <ProjectLink key={POST.title} to={`/portfolio/${POST.link}`}>{POST.title}</ProjectLink>)}
+                <div className={`fixed bg-cerise border-b-[1px] h-[54px]  top-0 left-0 laptop:w-[26vw] desktop:w-[21rem]  border-timberwolf dark:border-timberwolf border-opacity-50 dark:border-opacity-70 flex justify-end`}>
+                    {/* <div className="flex justify-around">
+                        <div className="bg-violet flex items-center mb-2 mt-3 px-4 rounded-md mr-3 py-3">
+                            <CategoryTitle text="text-platinum font-tommyMedium text-xl">Blog</CategoryTitle>
                         </div>
-                    ))}
-                    <div className="my-6">
-                        <CategoryTitle>Blog</CategoryTitle>
-                        <ProjectLink to={`/portfolio`}>Introduction</ProjectLink>
-                        {blogEntries.map((POST: any) => <ProjectLink key={POST.id} to={`/blog/${POST.slug}`}>{POST.data.title}</ProjectLink>)}
-                    </div>
-                    <div className="absolute bottom-0 left-0 right-0">
+                    </div> */}
+                </div>
+                 
+                <div className="">
+                   
+                    <ul className={`${showSideMenu ? 'relative' : 'hidden tablet:flex flex-col'}  p-1 pl-3  w-full ${showProjectList ? 'opacity-100' : 'opacity-0'} laptop:!opacity-100 duration-300 transition-all `}>
+                        <div className="mt-2">
+                            <ProjectLink to={`/portfolio`}>Introduction</ProjectLink>
+                        </div>
+                        {POST_CATEGORIES.map(cat => (
+                            <div key={cat.title} className="my-2">
+                                <CategoryTitle>{cat.title}</CategoryTitle>
+                                {POSTS[cat.title].map(POST => <ProjectLink key={POST.title} to={`/portfolio/${POST.link}`}>{POST.title}</ProjectLink>)}
+                            </div>
+                        ))}
+                        <div className="my-2">
+                            <CategoryTitle>Blog</CategoryTitle>
+                            {blogEntries.map((POST: any) => <ProjectLink key={POST.id} to={`/blog/${POST.slug}`}>{POST.data.title}</ProjectLink>)}
+                        </div>
+                        {/* <div className="absolute bottom-0 left-0 right-0">
                         <ul className="flex justify-between px-8 h-full py-2 my-2 font-chrono text-3xl text-raisinblack items-center">
-                            <li className="my-4">Login</li>
-                            {/* <li className="my-4 text-7xl font-semibold">{'<'}</li> */}
+                        <li className="my-4">Login</li>
+                        <li className="my-4 text-7xl font-semibold">{'<'}</li>
                         </ul>
-                    </div>
-                </ul>
+                        </div> */}
+                    </ul>
+                </div>
             </aside>
 
         </>
