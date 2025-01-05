f4930ad3414bad3aa5294607f81d05c99c15cec0
Agustin Rojas <agus.rojasmolina@gmail.com>
2025-01-04 22:03:14 -0300

    mobile

--- src/components/atoms/ProjectImages.tsx
+++ src/components/atoms/ProjectImages.tsx
@@ -1,112 +1,135 @@
 import React, { useEffect, useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import type { slide } from "astro/virtual-modules/transitions.js";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
 function ProjectImages({ images }: { images: string[] | undefined }) {
-    const [indexImage, setIndexImage] = useState<number>(0);
-    const [indexNextImage, setIndexNextImage] = useState<number>(0);
-    const [imageLoaded, setImageLoaded] = useState(false);
-    const [isCooldown, setCooldown] = useState(false);
+  const [indexImage, setIndexImage] = useState<number>(0);
+  const [indexNextImage, setIndexNextImage] = useState<number>(0);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [isCooldown, setCooldown] = useState(false);
+  const [openLightbox, setOpenLightbox] = useState(false);
+  const handleChangeImage = (idx: number) => {
+    if (!isCooldown) {
+      // Execute your logic here
+      setIndexImage(indexNextImage == 0 ? 0 : indexNextImage);
+      setIndexNextImage(idx);
+      setImageLoaded(true);
+      setTimeout(() => {
+        setImageLoaded(false);
+        setIndexImage(idx);
+      }, 150);
+      // Set cooldown to true
+      setCooldown(true);
 
-    const handleChangeImage = (idx: number) => {
-        if (!isCooldown) {
-            // Execute your logic here
-            setIndexImage(indexNextImage == 0 ? 0 : indexNextImage);
-            setIndexNextImage(idx);
-            setImageLoaded(true);
-            setTimeout(() => {
-                setImageLoaded(false);
-                setIndexImage(idx);
-            }, 150);
-            // Set cooldown to true
-            setCooldown(true);
+      // Set a timeout to reset the cooldown after a certain time (e.g., 2 seconds)
+      setTimeout(() => {
+        setCooldown(false);
+      }, 200); // Adjust the time as needed
+    }
+  };
 
-            // Set a timeout to reset the cooldown after a certain time (e.g., 2 seconds)
-            setTimeout(() => {
-                setCooldown(false);
-            }, 200); // Adjust the time as needed
+  useEffect(() => {
+    if (images) {
+      const interval = setInterval(() => {
+        if (images.length - 1 == indexImage) {
+          setIndexNextImage(0);
+        } else {
+          setIndexNextImage(indexImage + 1);
         }
-    };
+        setImageLoaded(true);
+        setTimeout(() => {
+          setImageLoaded(false);
+          if (images.length - 1 > indexImage) {
+            setIndexImage((prevState) => prevState + 1);
+          } else {
+            setIndexImage(0);
+          }
+        }, 150);
+      }, 3500);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [images, indexImage]);
 
-    
-    useEffect(() => {
-        if (images) {
-            const interval = setInterval(() => {
-                if ((images.length - 1) == indexImage) {
-                    setIndexNextImage(0)
-                } else {
-                    setIndexNextImage(indexImage + 1)
-                }
-                setImageLoaded(true)
-                setTimeout(() => {
-                    setImageLoaded(false);
-                    if ((images.length - 1) > indexImage) {
-                        setIndexImage((prevState) => prevState + 1)
-                    } else {
-                        setIndexImage(0);
-                    }
-                }, 150)
 
-            }, 3500)
-            return () => {
-                clearInterval(interval)
-            }
-        }
-    }, [images, indexImage])
-
-    return (
-        <div className="flex flex-col laptop:flex-row laptop:items-center w-[100%] mx-auto px-4 self-center not-prose gap-2">
-            <div className="w-full rounded-lg">
-                <div className="rounded-lg">
-                    <div className="relative h-full aspect-[4/5] tablet:aspect-video rounded-lg">
-                        {images && (
-                            <img
-                                alt={images[indexImage]}
-                                src={images[indexImage]}
-
-                                className={`absolute  w-full  rounded-lg object-cover tablet:object-cover  aspect-[4/5] tablet:aspect-video  transition-opacity opacity-100 ease-in-out ${imageLoaded ? "opacity-0" : "opacity-100"} `}
-                            />
-                        )}
-                        {images && (
-                            <img
-                                alt={images[indexNextImage]}
-                                src={images[indexNextImage]}
-
-                                className={`absolute w-full rounded-lg object-cover tablet:object-cover  aspect-[4/5] tablet:aspect-video  transition-opacity ease-in-out ${imageLoaded ? "opacity-100" : "opacity-0"}`}
-                            />
-                        )}
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="flex flex-col laptop:flex-row laptop:items-center w-[100%]  mx-auto tablet:px-4 self-center not-prose gap-2">
+        <div className="w-full rounded-lg ">
+          <div className="rounded-lg">
+            <div className="relative h-[12rem] tablet:h-full tablet:aspect-video rounded-lg">
+              {images && (
+                <img
+                  onClick={() => setOpenLightbox(true)}
+                  alt={images[indexImage]}
+                  src={images[indexImage]}
+                  className={`absolute  w-full h-[12rem] tablet:h-full  rounded-lg object-cover tablet:object-cover  tablet:aspect-video  transition-opacity opacity-100 ease-in-out ${
+                    imageLoaded ? "opacity-0" : "opacity-100"
+                  } `}
+                />
+              )}
+              {images && (
+                <img
+                  onClick={() => setOpenLightbox(true)}
+                  alt={images[indexNextImage]}
+                  src={images[indexNextImage]}
+                  className={`absolute w-full h-[12rem] tablet:h-full rounded-lg object-cover tablet:object-cover  tablet:aspect-video  transition-opacity ease-in-out ${
+                    imageLoaded ? "opacity-100" : "opacity-0"
+                  }`}
+                />
+              )}
             </div>
-            <div className="laptop:h-[100%] laptop:mx-0 w-[100%] laptop:w-[12.7%]  rounded-md">
-                <div className={`grid grid-cols-4 w-[75%] mx-auto laptop:mx-0 laptop:w-[100%] laptop:grid-cols-1 place-content-around laptop:place-content-center h-full`}>
-                    {images &&
-                        images?.map((image, idx) => (
-                            <button
-                                onClick={() => handleChangeImage(idx)}
-                                onMouseEnter={() => handleChangeImage(idx)}
-                                key={image}
-                                style={{ borderColor: 'transparent' }}
-                                className="mx-[0.2em] laptop:mx-0 aspect-square border-2 border-transparent  rounded-md  overflow-hidden hover:brightness-125 block"
-                            >
-                                <img
-                                    alt="imgs"
-                                    src={image}
-                                    className="object-fill scale-[3] "
-                                />
-                            </button>
-                        ))}
-                    {/* {images && images.length == 2 && (
-                        <>
-                            <span className="mx-[0.2em] laptop:mx-0 aspect-square  border-opacity-0 rounded-md overflow-hidden block"></span>
-                            <span className="mx-[0.2em] laptop:mx-0 aspect-square  border-opacity-0 rounded-md  overflow-hidden block"></span>
-                        </>
-                    )}
-                    {images && images.length == 3 && (
-                        <span className="mx-[0.2em] laptop:mx-0 aspect-square  rounded-md  overflow-hidden block"></span>
-                    )} */}
-                </div>
-            </div>
+          </div>
         </div>
-    );
+        <div className="laptop:h-[100%] laptop:mx-0 w-[100%] laptop:w-[12.7%]  rounded-md">
+          <div
+            className={`grid grid-cols-4 w-[75%] mx-auto laptop:mx-0 laptop:w-[100%] laptop:grid-cols-1 place-content-around laptop:place-content-center h-full`}
+          >
+            {images &&
+              images?.map((image, idx) => (
+                <button
+                  onClick={() => handleChangeImage(idx)}
+                  onMouseEnter={() => handleChangeImage(idx)}
+                  key={image}
+                  style={{ borderColor: "transparent" }}
+                  className="mx-[0.2em] laptop:mx-0 aspect-square border-2 border-transparent  rounded-md  overflow-hidden hover:brightness-125 block"
+                >
+                  <img
+                    alt="imgs"
+                    src={image}
+                    className="object-fill scale-[3] "
+                  />
+                </button>
+              ))}
+          </div>
+        </div>
+      </div>
+      <Lightbox
+        open={openLightbox}
+        plugins={[Zoom]}
+        controller={{ closeOnBackdropClick: true }}
+        close={() => setOpenLightbox(false)}
+        slides={images && images?.map((image) => ({ src: image}))}
+        styles={{ container: { backgroundColor: "rgba(0, 0, 0, .7)" } }}
+        animation={{ zoom: 500 }}
+          zoom={{
+            maxZoomPixelRatio: 1.4,
+            zoomInMultiplier: 2,
+            doubleTapDelay: 300,
+            doubleClickDelay: 300,
+            doubleClickMaxStops: 2,
+            keyboardMoveDistance: 50,
+            wheelZoomDistanceFactor: 100,
+            pinchZoomDistanceFactor: 100,
+            scrollToZoom: false,
+          }}
+      />
+    </>
+  );
 }
 
 export default ProjectImages;
--- src/components/organisms/SideMenu.tsx
+++ src/components/organisms/SideMenu.tsx
@@ -1,132 +1,194 @@
-import { useEffect, useRef } from "react"
-import { useStore } from "@nanostores/react"
-import CategoryTitle from "../atoms/CategoryTitle"
-import ProjectLink from "../atoms/ProjectLink"
-import {  POSTS, POST_CATEGORIES } from "@/config/posts"
-import logo from '@/assets/logo.png'
-import { showProjectListAtom, stateSideMenuAtom } from "@/store"
+import { useEffect, useRef } from "react";
+import { useStore } from "@nanostores/react";
+import CategoryTitle from "../atoms/CategoryTitle";
+import ProjectLink from "../atoms/ProjectLink";
+import { POSTS, POST_CATEGORIES } from "@/config/posts";
+import logo from "@/assets/logo.png";
+import { showProjectListAtom, stateSideMenuAtom } from "@/store";
 
+function SideMenu({
+  blogEntries,
+  pathname,
+}: {
+  blogEntries: any;
+  pathname: string;
+}) {
+  const refLinks = useRef<HTMLDivElement | any>(null);
+  const wrapperRef = useRef<any>(null);
+  const ulRef = useRef<any>(null);
+  const stateSideMenu = useStore(stateSideMenuAtom);
+  const showProjectList = useStore(showProjectListAtom);
 
-function SideMenu({ blogEntries, pathname }: { blogEntries: any, pathname: string }) {
-    const refLinks = useRef<HTMLDivElement | any>(null);
-    const wrapperRef = useRef<any>(null)
-    const ulRef = useRef<any>(null)
-    const stateSideMenu = useStore(stateSideMenuAtom)
-    const showProjectList = useStore(showProjectListAtom) 
-
-    useEffect(() => {
-        stateSideMenuAtom.set('hidden')
-    },[])
-    useEffect(() => {
-        function handleClickOutside(event: any) {
-            if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-                sideMenuHandler(true)
-            }
-            if (ulRef.current && ulRef.current.contains(event.target)) {
-                sideMenuHandler(true)
-            }
-        }
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-
-    }, [wrapperRef, stateSideMenu]);
-    //
-
-    const handlerMouseLinks = (event: any) => {
-        // BG ANIMATED OF FOOTER LINKS
-        const { left, top, width, height }: any =
-            event.target.getBoundingClientRect();
-        refLinks.current.style.opacity = "1";
-        refLinks.current.style.visibility = "visible";
-        refLinks.current?.style.setProperty("--left", `${left}px`);
-        refLinks.current?.style.setProperty("--top", `${top + 28}px`);
-        refLinks.current?.style.setProperty("--width", `${width}px`);
-        refLinks.current?.style.setProperty("--height", `${height - 22}px`);
-    };
-
-    const handlerLeaveLinks = () => {
-        // Leave mouse of the footer links
-        refLinks.current.style.opacity = "0";
-        refLinks.current.style.visibility = "hidden";
-    };
-
-    const sideMenuHandler = (fromOutside = false) => {
-      if (stateSideMenu === "hidden" && fromOutside == false) {
-        stateSideMenuAtom.set("opening");
-        setTimeout(() => {
-          stateSideMenuAtom.set("open");
-          showProjectListAtom.set(true);
-        }, 452);
-      } else if (stateSideMenu == 'open') {
-        stateSideMenuAtom.set("hiding");
-        showProjectListAtom.set(false);
-        setTimeout(() => {
-          stateSideMenuAtom.set("hidden");
-        }, 452);
+  useEffect(() => {
+    stateSideMenuAtom.set("hidden");
+  }, []);
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        sideMenuHandler(true);
       }
+      if (ulRef.current && ulRef.current.contains(event.target)) {
+        sideMenuHandler(true);
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, [wrapperRef, stateSideMenu]);
+  //
 
-    return (
-        <>
-            <aside
-                ref={wrapperRef}
-                className={`
-                ${stateSideMenu=='opening' && 'bg-raisinblack showSideMenu'} 
-                ${stateSideMenu=='hiding' && 'hideSideMenu bg-raisinblack'} 
-                ${stateSideMenu=='open' && 'block opacity-100  bg-raisinblack'} 
-                ${stateSideMenu=='hidden' && 'w-0.5 bg-opacity-0'} 
-                overflow-auto  z-30 h-full max-h-[100vh] laptop:w-[26vw] desktop:w-[21rem] fixed top-0 left-0 `}> {/*  overflow-y-scroll  */}
-                <div className="">
-                    <div className="">
-                        <ul className="flex justify-between w-[17rem] h-10 mx-auto  font-tommyMedium text-xl text-platinum items-center mt-8 widescreen:mt-10">
-                            <a
-                                href="/"
-                                className=" ml-4 z-50 tablet:mr-4 cursor-pointer"
-                            >
-                                <img
-                                    src={logo.src}
-                                    className="w-16 tablet:w-[3.9rem] drop-shadow-md transition-opacity-1 z-50"
-                                    alt="caracol"
-                                />
-                            </a>
-                            <a href="/blog" className={`${pathname.includes("/blog") && 'bg-cerisedark'} cursor-pointer p-2 rounded-t-md rounded-bl-md`}><li className="" onMouseEnter={handlerMouseLinks}
-                                onMouseLeave={handlerLeaveLinks} >Blog</li></a>
-                            <a href="/portfolio" className={`${pathname.includes("/portfolio") && 'bg-cerisedark cursor-pointer transition-colors'} p-2 mr-4 tablet:mr-2 laptop:mr-0 rounded-t-md rounded-br-md`}><li className="" onMouseEnter={handlerMouseLinks}
-                                onMouseLeave={handlerLeaveLinks} >Portfolio</li></a>
-                        </ul>
-                        <div
-                            ref={refLinks}
-                            className={`bg-cerisedark absolute left-[var(--left)] top-[var(--top)] z-50 h-[var(--height)] w-[var(--width)] rounded-md opacity-25 backdrop-blur-lg transition-all duration-300 ease-in-out`}
-                        ></div>
-                    </div>
-                    <ul className={`${stateSideMenu === 'opening' || stateSideMenu === 'open' || stateSideMenu == 'hiding' ? 'relative' : 'hidden tablet:flex flex-col'}  p-1 pl-3  w-full ${showProjectList ? 'opacity-100' : 'opacity-0'} laptop:!opacity-100 duration-300 transition-all `}>
+  const handlerMouseLinks = (event: any) => {
+    // BG ANIMATED OF FOOTER LINKS
+    const { left, top, width, height }: any =
+      event.target.getBoundingClientRect();
+    refLinks.current.style.opacity = "1";
+    refLinks.current.style.visibility = "visible";
+    refLinks.current?.style.setProperty("--left", `${left}px`);
+    refLinks.current?.style.setProperty("--top", `${top + 28}px`);
+    refLinks.current?.style.setProperty("--width", `${width}px`);
+    refLinks.current?.style.setProperty("--height", `${height - 22}px`);
+  };
 
-                        {pathname.includes("/portfolio") ? (<><div className="mt-2">
-                            <ProjectLink to={`/portfolio`} sideMenuHandler={sideMenuHandler}>Introduction</ProjectLink>
-                        </div>
-                            {POST_CATEGORIES.map(cat => (
-                                <div key={cat.title} className="my-2">
-                                    <CategoryTitle>{cat.title}</CategoryTitle>
-                                    {POSTS[cat.title].map(POST => <ProjectLink key={POST.title} sideMenuHandler={sideMenuHandler} to={`/portfolio/${POST.link}`}>{POST.title}</ProjectLink>)}
-                                </div>
-                            ))}</>)
-                            : <div className="my-2">
-                                <CategoryTitle>Blog</CategoryTitle>
-                                {blogEntries.map((POST: any) => <ProjectLink key={POST.id} sideMenuHandler={sideMenuHandler} to={`/blog/${POST.slug}`}>{POST.data.title}</ProjectLink>)}
-                            </div>
-                        }
+  const handlerLeaveLinks = () => {
+    // Leave mouse of the footer links
+    refLinks.current.style.opacity = "0";
+    refLinks.current.style.visibility = "hidden";
+  };
 
-                    </ul>
+  const sideMenuHandler = (fromOutside = false) => {
+    if (stateSideMenu === "hidden" && fromOutside == false) {
+      stateSideMenuAtom.set("opening");
+      setTimeout(() => {
+        stateSideMenuAtom.set("open");
+        showProjectListAtom.set(true);
+      }, 452);
+    } else if (stateSideMenu == "open") {
+      stateSideMenuAtom.set("hiding");
+      showProjectListAtom.set(false);
+      setTimeout(() => {
+        stateSideMenuAtom.set("hidden");
+      }, 452);
+    }
+  };
+
+  return (
+    <>
+      <aside
+        ref={wrapperRef}
+        className={`
+                ${stateSideMenu == "opening" && "bg-raisinblack showSideMenu"} 
+                ${stateSideMenu == "hiding" && "hideSideMenu bg-raisinblack"} 
+                ${
+                  stateSideMenu == "open" && "block opacity-100  bg-raisinblack"
+                } 
+                ${stateSideMenu == "hidden" && "w-0.5 bg-opacity-0"} 
+                overflow-auto  z-30 h-full max-h-[100vh] laptop:w-[26vw] desktop:w-[21rem] fixed top-0 left-0 `}
+      >
+        {" "}
+        {/*  overflow-y-scroll  */}
+        <div className="">
+          <div className="">
+            <ul className="flex justify-between w-[17rem] h-10 mx-auto  font-tommyMedium text-xl text-platinum items-center mt-8 widescreen:mt-10">
+              <a href="/" className=" ml-4 z-50 tablet:mr-4 cursor-pointer">
+                <img
+                  src={logo.src}
+                  className="w-16 tablet:w-[3.9rem] drop-shadow-md transition-opacity-1 z-50"
+                  alt="caracol"
+                />
+              </a>
+              <a
+                href="/blog"
+                className={`${
+                  pathname.includes("/blog") && "bg-cerisedark"
+                } cursor-pointer p-2 rounded-t-md rounded-bl-md`}
+              >
+                <li
+                  className=""
+                  onMouseEnter={handlerMouseLinks}
+                  onMouseLeave={handlerLeaveLinks}
+                >
+                  Blog
+                </li>
+              </a>
+              <a
+                href="/portfolio"
+                className={`${
+                  pathname.includes("/portfolio") &&
+                  "bg-cerisedark cursor-pointer transition-colors"
+                } p-2 mr-4 tablet:mr-2 laptop:mr-0 rounded-t-md rounded-br-md`}
+              >
+                <li
+                  className=""
+                  onMouseEnter={handlerMouseLinks}
+                  onMouseLeave={handlerLeaveLinks}
+                >
+                  Portfolio
+                </li>
+              </a>
+            </ul>
+            <div
+              ref={refLinks}
+              className={`bg-cerisedark absolute left-[var(--left)] top-[var(--top)] z-50 h-[var(--height)] w-[var(--width)] rounded-md opacity-25 backdrop-blur-lg transition-all duration-300 ease-in-out`}
+            ></div>
+          </div>
+          <ul
+            className={`${
+              stateSideMenu === "opening" ||
+              stateSideMenu === "open" ||
+              stateSideMenu == "hiding"
+                ? "relative"
+                : "hidden tablet:flex flex-col"
+            }  p-1 pl-3  w-full ${
+              showProjectList ? "opacity-100" : "opacity-0"
+            } laptop:!opacity-100 duration-300 transition-all `}
+          >
+            {pathname.includes("/portfolio") ? (
+              <>
+                <div className="mt-2">
+                  <ProjectLink
+                    to={`/portfolio`}
+                    sideMenuHandler={sideMenuHandler}
+                  >
+                    Introduction
+                  </ProjectLink>
                 </div>
-            </aside>
-
-        </>
-    )
+                {POST_CATEGORIES.map((cat) => (
+                  <div key={cat.title} className="my-2">
+                    <CategoryTitle>{cat.title}</CategoryTitle>
+                    {POSTS[cat.title].map((POST) => (
+                      <ProjectLink
+                        key={POST.title}
+                        sideMenuHandler={sideMenuHandler}
+                        to={`/portfolio/${POST.link}`}
+                      >
+                        {POST.title}
+                      </ProjectLink>
+                    ))}
+                  </div>
+                ))}
+              </>
+            ) : (
+              <div className="my-2">
+                <CategoryTitle>Posts</CategoryTitle>
+                {blogEntries.map((POST: any) => (
+                  <ProjectLink
+                    key={POST.id}
+                    sideMenuHandler={sideMenuHandler}
+                    to={`/blog/${POST.slug}`}
+                  >
+                    {POST.data.title}
+                  </ProjectLink>
+                ))}
+              </div>
+            )}
+          </ul>
+        </div>
+      </aside>
+    </>
+  );
 }
 
-
-export default SideMenu
+export default SideMenu;
